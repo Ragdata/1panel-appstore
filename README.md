41bbb1445ab93c9f8e45c6950c889a1de82ad75c
Ragdata <ragdata@users.noreply.github.com>
2025-12-30 21:24:04 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,163 @@
+<a name="top" href="https://github.com/ragdata" target="_blank"><img height="100" align="right" src="https://raw.githubusercontent.com/Ragdata/media/master/logo/Ragdata-64.svg" alt="1Panel-Appstore" /></a>
+
+<!-- [![Codacy grade][codacy-grade]][codacy-repo] -->
+![Pre-Release][pre-release]
+[![Version][version-badge]][release]
+
+<h1>
+
+[1Panel-Appstore][release]
+
+</h1>
+
+<div align="center">
+
+### _A Collection of Self-Hosted Apps for 1Panel_
+
+[![GitHub issues][issues-badge]][issues]
+![Last Commit][last-commit]
+[![AGPL][license-badge]][mit-license]
+<br />
+[![wakatime][wakatime-badge]][wakatime-repo]
+
+</div>
+
+<div align="center">
+
+<a href="https://bsky.app/profile/aever.au" target="_blank"><img alt="Bluesky" src="https://img.shields.io/badge/Bluesky-0085ff?style=flat-square&logo=bluesky&logoColor=white" /></a>
+<a href="mailto:github.discharge208@passfwd.com" target="_blank"><img alt="Bluesky" src="https://img.shields.io/badge/Email-00B4F0?style=flat-square&logo=maildotru&logoColor=white" /></a>
+<a href="https://discord.com/users/146165361333633024" target="_blank"><img alt="Discord" src="https://img.shields.io/badge/Discord-5865f2?style=flat-square&logo=discord&logoColor=white" /></a>
+
+</div>
+
+<hr />
+
+## [Requirements](#top) ❓
+
+## [Features](#top) ✨
+
+- 1-click update and synchronization of 1Panel's Official Appstore applications to your local environment
+- Sync apps from _any_ third-party repository to 1Panel's Local Appstore
+- Compatible with both public and private repositories under GitHub, GitLab, and Gitea
+- Sync multiple distributed repository applications to 1Panel's Local Appstore
+- Supports custom branches, clone depth, proxies, etc., for convenience and flexibility
+- Synchronization script's source code is completely open source, so you can extend and modify it as required
+
+[`^ Top`](#top)
+
+## [Installation](#top) 📂
+
+> [!info]
+> Add `local_appstore_sync_helper.sh` to a 1Panel scheduled task to run it periodically and automatically synchronize applications in this repository to your 1Panel Local Appstore.
+
+[`^ Top`](#top)
+
+## [Configuration](#top) 🚧
+
+
+[`^ Top`](#top)
+
+## [Resources](#top) 📖
+
+<details>
+<summary><strong>More Docker-Compose Collections</strong></summary>
+
+* [Official Docker Compose Samples](https://github.com/docker/awesome-compose)
+* [Funky Penguin's Geek Cookbok](https://github.com/geek-cookbook/geek-cookbook) &nbsp;&nbsp; <[Website](https://geek-cookbook.funkypenguin.co.nz/)> &nbsp;&nbsp; <[Premix](https://github.com/geek-cookbook/premix)>
+* [Haxxnet's Awesome Docker Compose Examples](https://github.com/Haxxnet/Compose-Examples) &nbsp;&nbsp; <[Website](https://haxxnet.github.io/Compose-Examples/)>
+* [Linuxserver.io Images](https://www.linuxserver.io/our-images)
+* [Docker Boilerplates](https://github.com/madeofpendletonwool/docker-boilerplates)
+* [DockSTARTer](https://github.com/GhostWriters/DockSTARTer)
+* [Docker Swarm Rocks](https://dockerswarm.rocks/)
+* [Docker Swarm Cookbook](https://github.com/Drauku/docker-swarm-cookbook)
+* [Awesome Stacks](https://github.com/ethibox/awesome-stacks)
+* [Selfhosted Apps Docker](https://github.com/DoTheEvo/selfhosted-apps-docker)
+
+
+</details>
+
+### [**Self-Hosting**](https://github.com/docker)
+
+* [Awesome Selfhosted](https://github.com/awesome-selfhosted/awesome-selfhosted) &nbsp;&nbsp; <[Website](https://awesome-selfhosted.net/)>
+* [The Homelab Wiki](https://thehomelab.wiki/)
+* [Mike Royal's Self-Hosting Guide](https://github.com/mikeroyal/Self-Hosting-Guide)
+* [Selfh.st](https://selfh.st/)
+* [Noted.lol](https://noted.lol)
+
+
+<br>
+
+[`^ Top`](#top)
+
+## [Security](#top) 🔐
+
+> [!warning]
+>If you discover any issue regarding the security of this project, please disclose that information responsibly by sending a [security advisory][advisory].  **PLEASE DO NOT CREATE AN ISSUE OR DISCUSSION TOPIC.**  You can read more about this project's security policies [HERE][security]
+
+While I always good security practices, 100% security can never be guaranteed in any software package.  `1Panel-Appstore` is provided AS IS, and without warranty.  You can find more details in the [LICENSE](LICENSE) file included with this repository.
+
+[`^ Top`](#top)
+
+## [License](#top) ⚖️
+
+<div align="center">
+
+[![MIT][license-badge]][mit-license]
+
+Copyright © 2025 Redeyed Technologies
+
+[_**CLICK HERE FOR THE FULL TEXT OF THIS LICENSE**_][mit-license]
+
+</div>
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+&nbsp;
+
+<div align="center">
+
+<a href="https://visitorbadge.io/status?path=https%3A%2F%2Fgithub.com%2Fragdata%2F1panel-appstore" target="_blank"><img alt="Visitors" src="https://api.visitorbadge.io/api/combined?path=https%3A%2F%2Fgithub.com%2Fragdata%2F1panel-appstore&countColor=%23d20000" /></a>
+<a href="https://github.com/Ragdata" target="_blank"><img alt="Ragdata" src="https://img.shields.io/badge/-Made_With_☕_By_Ragdata-D20000?style=for-the-badge" /></a>
+
+<h4>
+
+If you like this repository, please give it a ⭐ (it really does help)
+
+<img alt="GitHub repository stars" src="https://img.shields.io/github/stars/ragdata/1panel-appstore?style=social">
+
+</h4>
+
+Copyright &copy; 2025 Redeyed Technologies
+</div>
+
+[//]: # (############################################################)
+
+[release]: https://github.com/ragdata/1panel-appstore/releases
+<!-- [gh-pages]: https://ragdata.github.io/1panel-appstore/
+[repo]: https://github.com/ragdata/1panel-appstore -->
+
+[pre-release]: https://img.shields.io/badge/Status-Pre--Release-d20000?labelColor=31383f
+[issues-badge]: https://img.shields.io/github/issues-raw/ragdata/1panel-appstore?style=for-the-badge&logo=github
+[license-badge]: https://img.shields.io/badge/License-MIT-gold?style=for-the-badge
+[last-commit]: https://img.shields.io/github/last-commit/ragdata/1panel-appstore/master?style=for-the-badge
+[version-badge]: https://img.shields.io/badge/dynamic/yaml?url=https%3A%2F%2Fraw.githubusercontent.com%2Fragdata%2F1panel-appstore%2Fmaster%2F.releaserc&query=%24.version&prefix=v&label=Version&labelColor=31383f&color=cd4800
+
+[issues]: https://github.com/ragdata/1panel-appstore/issues
+[mit-license]: https://choosealicense.com/licenses/mit/
+
+[wakatime-badge]: https://wakatime.com/badge/user/7e04d9d4-3a44-495e-b622-69fdbafd036c/project/57b4a67e-9f5a-412d-996d-80dfb185c607.svg?style=for-the-badge
+[wakatime-repo]: https://wakatime.com/badge/user/7e04d9d4-3a44-495e-b622-69fdbafd036c/project/57b4a67e-9f5a-412d-996d-80dfb185c607
+
+[advisory]: https://github.com/ragdata/1panel-appstore/security/advisories/new
+[security]: https://github.com/ragdata/1panel-appstore/security/policy
+
+<!-- [all-contributors]: https://allcontributors.org
+[contributing]: https://github.com/ragdata/.github/blob/master/.github/CONTRIBUTING.md -->
+
+<!-- [ragdata-repo]: https://github.com/Ragdata -->
---
+++ local_appstore_sync_helper.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+# Just run or copy this script to the 1Panel -> scheduled task to run it regularly
+
+# Directory of the local app (if not installed by default, you need to create it manually)
+app_local_dir="/opt/1panel/resource/apps/local"
+
+# Appstore git repository URL (required)
+# git_repo_url="https://github.com/xxxily/local-appstore-for-1Panel"
+#git_repo_url="https://github.com/okxlin/appstore"
+git_repo_url="https://github.com/qwenode/1panel-appstore"
+
+# Access token for the git repository (optional)
+# Using an access token is recommended to reduce the risk of leaking account secrets
+git_access_token=""
+
+# Username for accessing the git repository, used for private repositories (optional)
+git_username=""
+# Password for accessing the git repository, used for private repositories (optional)
+git_password=""
+
+# Specify the branch to clone (optional)
+git_branch=""
+# Specify the clone depth (optional, default is 1 for shallow clone)
+git_depth=1
+
+# Whether to empty the local app directory before pulling the remote repository (optional)
+clean_local_app=false
+# Whether to empty the remote app cache data after pulling (optional)
+clean_remote_app_cache=false
+
+# Setup an agent to use when pulling the remote repository (optional)
+proxyUrl=""
+# Example setup:
+# proxyUrl="http://127.0.0.1:7890"
+# proxyUrl="socks5://127.0.0.1:7890"
+# proxyUrl="socks5://user:password@host:port"
+
+# Clone a remote Appstore project to a local working directory
+work_dir="/opt/1panel_hepler"
+
+set -e
+
+mkdir -p "$work_dir/logs"
+log_file="$work_dir/logs/local_appstore_sync_helper_$(date +"%Y-%m-%d").log"
+logs() {
+  local message="$1"
+
+  if [ -n "$log_file" ]; then
+    mkdir -p "$(dirname "$log_file")"
+    if [ $? -eq 0 ]; then
+      echo -e "[$(date +"%Y-%m-%d %H:%M:%S")] $message"
+      echo "[$(date +"%Y-%m-%d %H:%M:%S")] $message" >>"$log_file"
+      return
+    fi
+  fi
+
+  echo -e "$message"
+}
+
+# Function: url_encode
+# Parameters:
+#   - url: The string to be encoded
+# Return:
+#   URL-encoded string
+function url_encode() {
+  local string=$1
+  local length="${#string}"
+  local url_encoded_string=""
+  local c
+
+  for ((i = 0; i < length; i++)); do
+    c=${string:i:1}
+    case "$c" in
+    [a-zA-Z0-9.~_-]) url_encoded_string+=$c ;;
+    *) url_encoded_string+=$(printf '%%%02X' "'$c") ;;
+    esac
+  done
+
+  echo "$url_encoded_string"
+}
+
+# A function to replace or remove the protocol part of a URL
+replace_protocol() {
+  local url=$1
+  local replacement=$2
+
+  # If no remplacement is provided, remove the protocol part
+  if [[ -z $replacement ]]; then
+    local new_url=$(echo $url | sed "s/http:\/\///" | sed "s/https:\/\///")
+  else
+    local new_url=$(echo $url | sed "s/http:\/\//${replacement}/" | sed "s/https:\/\//${replacement}/")
+  fi
+
+  # 输出替换后的URL
+  echo $new_url
+}
+
+# Function: clone_git_repo
+# Parameters:
+#   - url: URL of the git repository
+#   - username: Username (optional)
+#   - password: Password (optional)
+#   - access_token: Access token (optional)
+#   - branch: Clone branch (optional)
+#   - depth: Clone depth (optional)
+function clone_git_repo() {
+  local url=$1
+  local username=$2
+  local password=$3
+  local access_token=$4
+  local branch=$5
+  local depth=$6
+
+  branch=${branch:+--branch $branch}
+  depth=${depth:+--depth $depth}
+
+  echo "branch: $branch, depth: $depth"
+
+  if [[ -n $access_token ]]; then
+    echo "use access_token to clone"
+    local fix_url=$(replace_protocol "$url")
+    git clone "https://oauth2:$access_token@$fix_url" $branch $depth
+  elif [[ -n $username && -n $password ]]; then
+    local encoded_username=$(url_encode "$username")
+    local encoded_password=$(url_encode "$password")
+    local fix_url=$(replace_protocol "$url")
+
+    # echo "use username and password to clone, encoded_username: $encoded_username, encoded_password: $encoded_password, fix_url: $fix_url"
+    echo "use username and password to clone"
+
+    git clone "https://$encoded_username:$encoded_password@$fix_url" $branch $depth
+  else
+    echo "use default clone"
+    git clone "$url" $branch $depth
+  fi
+}
+
+# Cancel proxy
+function proxy_off() {
+  unset http_proxy
+  unset https_proxy
+  unset ALL_PROXY
+  unset no_proxy
+  logs "Proxy Off"
+}
+
+# Enable proxy
+function proxy_on() {
+  proxy_url="http://127.0.0.1:7890"
+  match_str="://"
+
+  if [ -n "$1" ]; then
+    if [[ $1 =~ $match_str ]]; then
+      proxy_url=$1
+    else
+      logs "Incorrect proxy_url, use defualt proxy_url"
+      return
+    fi
+  fi
+
+  export http_proxy=$proxy_url
+  export https_proxy=$proxy_url
+  export ALL_PROXY=$proxy_url
+  export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
+  logs "Proxy On $proxy_url"
+}
+
+function scriptInfo() {
+  echo ""
+  logs "##################################################################"
+  logs "#    Name: local appstore sync helper for 1Panel                 #"
+  logs "# Version: v1.0.0                                                #"
+  logs "#  Author: xxxily                                                #"
+  logs "#  Github: https://github.com/xxxily/local-appstore-for-1Panel   #"
+  logs "##################################################################"
+  echo ""
+}
+
+function main() {
+  scriptInfo
+
+  if [ ! -d "$app_local_dir" ]; then
+    logs "If the app directory of 1Panel is not detected, check whether 1Panel is installed corrector, or modify the app_local_dir variable"
+    exit 1
+  fi
+
+  # Check if the address ends with .git and autocomplete if not
+  if [[ "$git_repo_url" != *".git" ]]; then
+    git_repo_url="${git_repo_url}.git"
+  fi
+
+  local repo_username=""
+  local repo_projectname=""
+
+  # Use regular expressions to match the username and project name from the git_repo_url
+  if [[ $git_repo_url =~ .*\/(.*)\/(.*)\.git ]]; then
+    repo_username=${BASH_REMATCH[1]}
+    repo_projectname=${BASH_REMATCH[2]}
+    # logs "username: $repo_username"
+    # logs "project name: $repo_projectname"
+  fi
+
+  if [ -z "$repo_username" ] || [ -z "$repo_projectname" ]; then
+    logs "If you can't extract the username and project name, check that the address provided by git_repo_url is correct"
+    exit 1
+  fi
+
+  mkdir -p "$work_dir/temp"
+
+  local repo_user_dir="$work_dir/temp/$repo_username"
+  local repo_dir="$repo_user_dir/$repo_projectname"
+
+  # The value of the clean_remote_app_cache variable decides whether to clear the remote app cache data
+  if [ "$clean_remote_app_cache" = true ] && [ -d "$repo_dir" ]; then
+    rm -rf "$repo_dir"
+    logs "Remote cache data cleared"
+  fi
+
+  # The value of the proxyURL variable determines whether to enable the proxy
+  if [ -n "$proxyUrl" ]; then
+    proxy_on "$proxyUrl"
+  fi
+
+  # Clone or pull the latest code from the remote repository
+  logs "Prepare to get the latest code from the repository：$git_repo_url"
+  if [ -d "$repo_dir" ]; then
+    logs "Perform git pull"
+    cd "$repo_dir"
+
+    # Force pull the latest code
+    git pull --force 2>>"$log_file"
+  else
+    logs "Perform git clone"
+    mkdir -p "$repo_user_dir"
+    cd "$repo_user_dir"
+
+    clone_git_repo "$git_repo_url" "$git_username" "$git_password" "$git_access_token" "$git_branch" "$git_depth" 2>>"$log_file"
+  fi
+
+  logs "Latest repository code obtained"
+
+  if [ ! -d "$repo_dir/apps" ]; then
+    logs "The apps directory is not detected - check if the remote repository is correct"
+    exit 1
+  fi
+
+  # The value of the clean_local_app variable decides whether to clear the local app directory
+  if [ "$clean_local_app" = true ]; then
+    rm -rf "$app_local_dir"/*
+    logs "The local app directory has been cleared"
+  fi
+
+  # Copy all apps in the remote apps directory to the local app directory
+  cd "$repo_dir"
+  cp -rf apps/* "$app_local_dir"
+
+  pwd
+  ls -lah
+  du -sh
+
+  # The value of the clean_remote_app_cache variable decides whether to clear the remote app cache data
+  if [ "$clean_remote_app_cache" = true ]; then
+    rm -rf "$repo_dir"
+  fi
+
+  if [ -n "$proxyUrl" ]; then
+    proxy_off
+  fi
+
+  logs "1Panel local app synchronization completed, enjoy it!"
+}
+
+main "$@"
